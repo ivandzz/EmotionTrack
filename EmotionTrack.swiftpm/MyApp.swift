0d61227e9846c47d884b8cf07da7f8417131fde5
ivandzz <ivan.dzhul@gmail.com>
2024-11-10 14:00:23 +0200

    refactor: Refactored Array extension

--- EmotionTrack.swiftpm/MyApp.swift
+++ EmotionTrack.swiftpm/MyApp.swift
@@ -1,20 +1,5 @@
 import SwiftUI
 
-//code from https://www.hackingwithswift.com/example-code/language/how-to-remove-duplicate-items-from-an-array
-extension Array where Element: Hashable {
-    func removingDuplicates() -> [Element] {
-        var addedDict = [Element: Bool]()
-        
-        return filter {
-            addedDict.updateValue(true, forKey: $0) == nil
-        }
-    }
-    
-    mutating func removeDuplicates() {
-        self = self.removingDuplicates()
-    }
-}
-
 @main
 struct MyApp: App {
     var body: some Scene {
